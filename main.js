29788ae92d0806d55a5c6ee84bbf4cd803f45f06
sam <samandarusmanov1@gmail.com>
2023-01-10 21:53:18 +0500

    add video player

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ tabs.forEach((tab, index) => {
         panels[index].classList.add('active')
     })
 })
+
+
+// Load the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Replace the 'ytplayer' element with an <iframe> and
+// YouTube player after the API code downloads.
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+        height: '360',
+        width: '640',
+        videoId: '5uZlfK5L0bk'
+    });
+}
