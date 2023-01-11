b7e5d31d66eef4baf729643fc1298dc6cb1f0996
Dalisalar <65603871+Dalisalar@users.noreply.github.com>
2023-01-11 04:13:35 +0300

    Lightweight embedded youtube player

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+function labnolIframe(div) {
+    var iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'https://www.youtube.com/embed/' + div.dataset.id + '?autoplay=1&rel=0');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allowfullscreen', '1');
+    iframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
+    div.parentNode.replaceChild(iframe, div);
+}
+
+function initYouTubeVideos() {
+    var playerElements = document.getElementsByClassName('youtube-player');
+    for (var n = 0; n < playerElements.length; n++) {
+        var videoId = playerElements[n].dataset.id;
+        var div = document.createElement('div');
+        div.setAttribute('data-id', videoId);
+        var thumbNode = document.createElement('img');
+        thumbNode.src = '//i.ytimg.com/vi/ID/hqdefault.jpg'.replace('ID', videoId);
+        div.appendChild(thumbNode);
+        var playButton = document.createElement('div');
+        playButton.setAttribute('class', 'play');
+        div.appendChild(playButton);
+        div.onclick = function() {
+            labnolIframe(this);
+        };
+        playerElements[n].appendChild(div);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', initYouTubeVideos);
+
 let tabs = document.querySelectorAll('.button'),
     panels = document.querySelectorAll('.panel');
 
@@ -20,6 +50,7 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 // Replace the 'ytplayer' element with an <iframe> and
 // YouTube player after the API code downloads.
 var player;
+
 function onYouTubePlayerAPIReady() {
     player = new YT.Player('ytplayer', {
         height: '360',
