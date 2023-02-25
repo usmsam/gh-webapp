03c236f7e52085c77bb9aa9648f72b640157e96a
sam <samandarusmanov1@gmail.com>
2023-02-25 13:52:19 +0500

    add all videos playlist

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
-let tabs = document.querySelectorAll('.button'),
-    panels = document.querySelectorAll('.panel');
+let tabs = document.querySelectorAll(".button"),
+  panels = document.querySelectorAll(".panel");
 
 tabs.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
-        tabs.forEach(tab => tab.classList.remove('active'))
-        tab.classList.add('active')
-        panels.forEach(panel => panel.classList.remove('active'));
-        panels[index].classList.add('active')
-    })
-})
-
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tab.classList.add("active");
+    panels.forEach((panel) => panel.classList.remove("active"));
+    panels[index].classList.add("active");
+  });
+});
 
 // Load the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // Replace the 'ytplayer' element with an <iframe> and
 // YouTube player after the API code downloads.
-var player;
+const data = [
+  "5uZlfK5L0bk",
+  "RM6Iuz8IrDk",
+  "uKySmivbcI0",
+  "wEa3JBzzKVA",
+  "kB9IGpmvUQM",
+  "6o2xxhgaGiI",
+  "4B8AbL3jGlU",
+  "p84uGg-2WrU",
+  "DMi1o9KPqxc",
+  "ZUFv5BPzwp8",
+];
+let videosBox = document.querySelector(".panel-videos");
+
 function onYouTubePlayerAPIReady() {
-    player = new YT.Player('ytplayer', {
-        height: '360',
-        width: '640',
-        videoId: '5uZlfK5L0bk'
+  data.forEach((el) => {
+    let div = document.createElement("div");
+    div.setAttribute("id", el);
+    div.setAttribute("class", "videoElement");
+    videosBox.appendChild(div);
+
+    new YT.Player(el, {
+      height: "360",
+      width: "640",
+      videoId: el,
     });
+  });
 }
