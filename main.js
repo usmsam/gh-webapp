97522823886a896b083928b545aba74600b9ac39
sam <samandarusmanov1@gmail.com>
2023-02-26 22:36:37 +0500

    fix js logic with yt iframe

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
 function labnolIframe(div) {
-  var iframe = document.createElement('iframe');
-  iframe.setAttribute('src', 'https://www.youtube.com/embed/' + div.dataset.id + '?autoplay=1&rel=0');
-  iframe.setAttribute('frameborder', '0');
-  iframe.setAttribute('allowfullscreen', '1');
-  iframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
+  var iframe = document.createElement("iframe");
+  iframe.setAttribute(
+    "src",
+    "https://www.youtube.com/embed/" + div.dataset.id + "?autoplay=1&rel=0"
+  );
+  iframe.setAttribute("frameborder", "0");
+  iframe.setAttribute("allowfullscreen", "1");
+  iframe.setAttribute(
+    "allow",
+    "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+  );
   div.parentNode.replaceChild(iframe, div);
 }
 
 function initYouTubeVideos() {
-  var playerElements = document.getElementsByClassName('youtube-player');
+  var playerElements = document.getElementsByClassName("youtube-player");
   for (var n = 0; n < playerElements.length; n++) {
-      var videoId = playerElements[n].dataset.id;
-      var div = document.createElement('div');
-      div.setAttribute('data-id', videoId);
-      var thumbNode = document.createElement('img');
-      thumbNode.src = '//i.ytimg.com/vi/ID/hqdefault.jpg'.replace('ID', videoId);
-      div.appendChild(thumbNode);
-      var playButton = document.createElement('div');
-      playButton.setAttribute('class', 'play');
-      div.appendChild(playButton);
-      div.onclick = function() {
-          labnolIframe(this);
-      };
-      playerElements[n].appendChild(div);
+    var videoId = playerElements[n].dataset.id;
+    var div = document.createElement("div");
+    div.setAttribute("data-id", videoId);
+    var thumbNode = document.createElement("img");
+    thumbNode.src = "//i.ytimg.com/vi/ID/hqdefault.jpg".replace("ID", videoId);
+    div.appendChild(thumbNode);
+    var playButton = document.createElement("div");
+    playButton.setAttribute("class", "play");
+    div.appendChild(playButton);
+    div.onclick = function () {
+      labnolIframe(this);
+    };
+    playerElements[n].appendChild(div);
   }
 }
 
-document.addEventListener('DOMContentLoaded', initYouTubeVideos);
+document.addEventListener("DOMContentLoaded", initYouTubeVideos);
 let tabs = document.querySelectorAll(".button"),
   panels = document.querySelectorAll(".panel");
 
@@ -47,31 +53,20 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // Replace the 'ytplayer' element with an <iframe> and
 // YouTube player after the API code downloads.
-const data = [
-  "5uZlfK5L0bk",
-  "RM6Iuz8IrDk",
-  "uKySmivbcI0",
-  "wEa3JBzzKVA",
-  "kB9IGpmvUQM",
-  "6o2xxhgaGiI",
-  "4B8AbL3jGlU",
-  "p84uGg-2WrU",
-  "DMi1o9KPqxc",
-  "ZUFv5BPzwp8",
-];
-let videosBox = document.querySelector("#youtube-player");
 
 function onYouTubePlayerAPIReady() {
-  data.forEach((el) => {
-    let div = document.createElement("div");
-    div.setAttribute("id", el);
-    div.setAttribute("class", "videoElement");
-    videosBox.appendChild(div);
+  let videosBox = document.querySelectorAll(".youtube-player");
+  videosBox.forEach((el) => {
+    let id = el.getAttribute("id");
 
-    new YT.Player(el, {
+    let div = document.createElement("div");
+    div.setAttribute("id", id + "-player");
+    el.appendChild(div);
+
+    new YT.Player(id + "-player", {
       height: "360",
       width: "640",
-      videoId: el,
+      videoId: id,
     });
   });
 }
